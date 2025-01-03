f2bbf6080d9eee4d3bc417039f6e1f3b63f517bf
Raghu K <raghuk.vit@gmail.com>
2025-01-03 07:21:52 +0530

    RoboSHop Project

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -25,8 +25,38 @@ variable "secrets" {
         shipping_endpoint  = "http://shipping-dev.azdevopsb82.online:8080"
       }
       catalogue = {
-        MONGO = "true"
+        MONGO     = "true"
         MONGO_URL = "mongodb://mongodb-dev.azdevopsb82.online:27017/catalogue"
+      }
+      user = {
+        MONGO     = "true"
+        REDIS_URL = "redis://redis-dev.azdevopsb82.online:6379"
+        MONGO_URL = "mongodb://mongodb-dev.azdevopsb82.online:27017/users"
+      }
+      cart = {
+        REDIS_HOST     = "redis-dev.azdevopsb82.online"
+        CATALOGUE_HOST = "catalogue-dev.azdevopsb82.online"
+        CATALOGUE_PORT = "8080"
+      }
+      shipping = {
+        CART_ENDPOINT = "cart-dev.azdevopsb82.online:8080"
+        DB_HOST       = "mysql-dev.azdevopsb82.online"
+      }
+      payment = {
+        CART_HOST = "cart-dev.azdevopsb82.online"
+        CART_PORT = "8080"
+        USER_HOST = "user-dev.azdevopsb82.online"
+        USER_PORT = "8080"
+        AMQP_HOST = "rabbitmq-dev.azdevopsb82.online"
+        AMQP_USER = "roboshop"
+        AMQP_PASS = "roboshop123"
+      }
+      mysql = {
+        MYSQL_ROOT_PASSWORD = "RoboShop@1"
+      }
+      rabbitmq = {
+        AMQP_USER = "roboshop"
+        AMQP_PASS = "roboshop123"
       }
     }
   }
