6b44a4dcd8561f0dd54a59f9eb923864d1800267
Raghu K <raghuk.vit@gmail.com>
2025-01-03 07:02:29 +0530

    RoboSHop Project

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -19,6 +19,10 @@ variable "secrets" {
     roboshop-dev = {
       frontend  = {
         catalogue_endpoint = "http://catalogue-dev.azdevopsb82.online:8080"
+        cart_endpoint = "http://cart-dev.azdevopsb82.online:8080"
+        user_endpoint = "http://user-dev.azdevopsb82.online:8080"
+        payment_endpoint = "http://payment-dev.azdevopsb82.online:8080"
+        shipping_endpoint = "http://shipping-dev.azdevopsb82.online:8080"
       }
       #catalogue = {}
     }
