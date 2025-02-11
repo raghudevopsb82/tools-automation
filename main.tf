229abf14b3b5e6d9b923dcb686d929358d44a838
Raghu K <raghuk.vit@gmail.com>
2025-02-11 07:10:13 +0530

    RoboSHop Project

--- main.tf
+++ main.tf
@@ -41,7 +41,4 @@ provider "azurerm" {
   subscription_id = "7b6c642c-6e46-418f-b715-e01b2f871413"
 }
 
-output "vm" {
-  value = module.vm
-}
 
--- vm-module/main.tf
+++ vm-module/main.tf
@@ -120,15 +120,11 @@ resource "azurerm_virtual_machine" "main" {
 
 }
 
-output "vm" {
-  value = azurerm_virtual_machine.main.identity
+resource "azurerm_role_assignment" "role-assignment" {
+  count                = var.role_definition_name == null ? 0 : 1
+  scope                = data.azurerm_resource_group.main.id
+  role_definition_name = var.role_definition_name
+  principal_id         = azurerm_virtual_machine.main.identity[0].principal_id
 }
-
-# resource "azurerm_role_assignment" "role-assignment" {
-#   count                = var.role_definition_name == null ? 0 : 1
-#   scope                = data.azurerm_resource_group.main.id
-#   role_definition_name = var.role_definition_name
-#   principal_id         = azurerm_virtual_machine.main.identity.principal_id
-# }
 
 
