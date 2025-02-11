b339171de7d81b638af7ee89e97e012c5d0f9ae9
Raghu K <raghuk.vit@gmail.com>
2025-02-11 07:13:23 +0530

    RoboSHop Project

--- vm-module/main.tf
+++ vm-module/main.tf
@@ -121,6 +121,7 @@ resource "azurerm_virtual_machine" "main" {
 }
 
 resource "azurerm_role_assignment" "role-assignment" {
+  depends_on           = [azurerm_virtual_machine.main]
   count                = var.role_definition_name == null ? 0 : 1
   scope                = data.azurerm_resource_group.main.id
   role_definition_name = var.role_definition_name
