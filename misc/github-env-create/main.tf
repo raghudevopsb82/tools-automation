5ee6b63c92ae152feca3b15eeb1e2239be9e6536
Raghu K <raghuk.vit@gmail.com>
2025-02-13 07:23:09 +0530

    RoboSHop Project

--- misc/github-env-create/main.tf
+++ misc/github-env-create/main.tf
@@ -9,3 +9,17 @@ terraform {
 
 # Configure the GitHub Provider
 provider "github" {}
+
+data "github_user" "current" {
+  username = "r-devops"
+}
+
+resource "github_repository_environment" "env" {
+  environment         = "DEV"
+  repository          = "raghudevopsb83/roboshop-cart"
+  prevent_self_review = false
+  reviewers {
+    users = [data.github_user.current.id]
+  }
+}
+
