5af19a22c74a9530e63ac7de78084bbcdcbfafcf
Raghu K <raghuk.vit@gmail.com>
2025-02-14 06:15:17 +0530

    RoboSHop Project

--- misc/github-env-create/main.tf
+++ misc/github-env-create/main.tf
@@ -7,6 +7,16 @@ terraform {
   }
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "project-setup-1"
+    storage_account_name = "d82tfstates"
+    container_name       = "github-env"
+    key                  = "terraform.tfstate"
+
+  }
+}
+
 # Configure the GitHub Provider
 provider "github" {
   owner = "raghudevopsb82"
@@ -16,12 +26,45 @@ data "github_user" "current" {
   username = "r-devops"
 }
 
-resource "github_repository_environment" "env" {
+variable "repos" {
+  default = ["cart", "catalogue", "user", "shipping", "payment", "frontend"]
+}
+
+resource "github_repository_environment" "dev" {
+  count               = length(var.repos)
   environment         = "DEV"
-  repository          = "roboshop-cart"
+  repository          = var.repos[count.index]
+  prevent_self_review = false
+}
+
+resource "github_repository_environment" "qa" {
+  count               = length(var.repos)
+  environment         = "QA"
+  repository          = var.repos[count.index]
   prevent_self_review = false
   reviewers {
     users = [data.github_user.current.id]
   }
 }
+
+resource "github_repository_environment" "uat" {
+  count               = length(var.repos)
+  environment         = "UAT"
+  repository          = var.repos[count.index]
+  prevent_self_review = false
+  reviewers {
+    users = [data.github_user.current.id]
+  }
+}
+
+resource "github_repository_environment" "prod" {
+  count               = length(var.repos)
+  environment         = "PROD"
+  repository          = var.repos[count.index]
+  prevent_self_review = false
+  reviewers {
+    users = [data.github_user.current.id]
+  }
+}
+
 
