2eb89ee32f062888c9f8f667e2a9f06184edf4f4
SVR Kalluru <raghuk.vit@gmail.com>
2025-02-26 06:45:02 +0530

    RoboShop

--- misc/vault-secrets/provider.tf
+++ misc/vault-secrets/provider.tf
@@ -1,12 +1,13 @@
 provider "vault" {
-  address = "http://vault-internal.azdevopsb82.online:8200"
+  #address = "http://vault-internal.azdevopsb82.online:8200"
+  address = "http://10.1.0.8:8200"
   token   = var.token
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "project-setup-1"
-    storage_account_name = "d82tfstates"
+    storage_account_name = "d82tfstatesnew"
     container_name       = "vault-tf-states"
     key                  = "terraform.tfstate"
 
@@ -15,7 +16,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "7b6c642c-6e46-418f-b715-e01b2f871413"
+  subscription_id = "a906d619-0839-4738-a908-227a8b69d458"
 }
 
 variable "token" {}
